cb39f95f0adad33c3dcf19a7c75529ae1e046615
thiyagarajan.n@adappt.ai <thiyagarajan.n@adappt.ai>
2022-12-22 18:58:47 +0530

    Add project files.

---
+++ BlazorTranslator/Data/TranslationService.cs
@@ -0,0 +1,80 @@
+﻿using BlazorTranslator.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTranslator.Data
+{
+    public class TranslationService
+    {
+        static string subscriptionKey;
+        static string apiEndpoint;
+        static string location;
+
+        public TranslationService()
+        {
+            //edb32a048fb249109cbe941cd678981
+           // subscriptionKey = "e380526a44b14b7b8e3d0a14455c783a";
+            subscriptionKey = "e115586ec0904934a3bbe1699766664b";
+            apiEndpoint = "https://api.cognitive.microsofttranslator.com/";
+            location = "southeastasia";
+        }
+
+        public async Task<TranslationResult[]> GetTranslatation(string textToTranslate, string targetLanguage)
+        {
+            string route = $"translate?api-version=3.0&to={targetLanguage}";
+            string requestUri = apiEndpoint + route;
+
+            string result = await TranslateText(requestUri, textToTranslate);
+
+            TranslationResult[] translationResult = JsonConvert.DeserializeObject<TranslationResult[]>(result);
+
+            return translationResult;
+        }
+
+        static async Task<string> TranslateText(string requestUri, string inputText)
+        {
+            string result = string.Empty;
+            object[] body = new object[] { new { Text = inputText } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using var client = new HttpClient();
+            using var request = new HttpRequestMessage();
+
+            request.Method = HttpMethod.Post;
+            request.RequestUri = new Uri(requestUri);
+            request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            request.Headers.Add("Ocp-Apim-Subscription-Region", location);
+
+            HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsStringAsync();
+            }
+
+            return result;
+        }
+
+        public async Task<AvailableLanguage> GetAvailableLanguages()
+        {
+            string endpoint = "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0&scope=translation";
+
+            var client = new HttpClient();
+            using var request = new HttpRequestMessage();
+            request.Method = HttpMethod.Get;
+            request.RequestUri = new Uri(endpoint);
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+
+            string result = await response.Content.ReadAsStringAsync();
+            AvailableLanguage deserializedOutput = JsonConvert.DeserializeObject<AvailableLanguage>(result);
+
+            return deserializedOutput;
+        }
+    }
+}
---
+++ BlazorTranslator/Pages/Translator.razor.cs
@@ -0,0 +1,49 @@
+﻿using BlazorTranslator.Data;
+using BlazorTranslator.Models;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorTranslator.Pages
+{
+    public class TranslatorBase : ComponentBase
+    {
+        [Inject]
+        protected TranslationService translationService { get; set; }
+
+        protected string inputLanguage { get; set; } = default;
+        protected string outputLanguage { get; set; } = default;
+        protected string inputText { get; set; } = default;
+        protected string outputText { get; set; } = default;
+        protected Dictionary<string, LanguageDetails> LanguageList = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            AvailableLanguage availableLanguages = await translationService.GetAvailableLanguages();
+            LanguageList = availableLanguages.Translation;
+        }
+
+        protected void SelectLanguage(ChangeEventArgs languageEvent)
+        {
+            outputLanguage = languageEvent.Value.ToString();
+        }
+
+        protected async Task Translate()
+        {
+            if (!string.IsNullOrEmpty(outputLanguage))
+            {
+                TranslationResult[] translations = await translationService.GetTranslatation(inputText, outputLanguage);
+
+                if (translations is not null)
+                {
+                    outputText = translations[0].Translations[0].Text;
+                    inputLanguage = translations[0].DetectedLanguage.Language;
+                }
+                else
+                {
+                    outputText = "An error occurred in translation. Please try again.";
+                }
+            }
+        }
+    }
+}
